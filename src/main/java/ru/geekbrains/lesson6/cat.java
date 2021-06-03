5b254be796617ded936e298ff1edbdcc56f76e07
geekbrains <info@geekbrains.ru>
2021-06-03 21:25:28 +0700

    Homeworck new

---
+++ src/main/java/ru/geekbrains/lesson6/cat.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson6;
+
+class cat extends Animals {
+
+   cat(double animalDistanceRun, double animalDistanceJump, double animalDistanceSwim) {
+        super(animalDistanceRun, animalDistanceJump, animalDistanceSwim);
+    }
+}
